994304450f7b4255d68a0134b2906f3d027268bf
RasikaSonavane <rsika2014@gmail.com>
2021-10-04 15:26:35 +0530

    assignment in progress

---
+++ buildObjects.js
@@ -0,0 +1,21 @@
+//Q1- Build JavaScript Objects
+var myDog ={name:"Oxy", legs:4, tails:1, friends:[ "ATy", "Billy", "Kitty"]}
+
+//Q2- Accessing Object Properties with Dot Notation
+var testObj ={hatValue :"ballcap", shirtValue:"jersey"}
+var hat=testObj.hatValue
+var shirt=testObj.shirtValue
+
+//Q3- Accessing Object Properties with Bracket Notation
+var testObj ={entreeValue:"hamburger" ,drinkValue:"water"}
+entree =testObj["entreeValue"]
+drink=testObj['drinkValue']
+
+//Q4- Accessing Object Properties with Bracket Notation
+//Q.3 and Q.4 Repeated
+
+//Q5- Accessing Object Properties with Variables
+
+
+
+
